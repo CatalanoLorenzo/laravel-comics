e7b523994a456191ef8126736ee72f0ef30a7023
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-05-23 16:09:27 +0200

    initial commits

---
+++ config/voice_navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Mime\CharacterStream;
+
+return[
+
+    "voices"=>["CHARACTERS"=>"Characters",
+    "COMICS"=>"Comics",
+    "MOVIES"=>"Movies",
+    "TV"=>"Tv",
+    "GAMES"=>"Games",
+    "COLLECTIBLES"=>"Collectibles",
+    "VIDEOS"=>"Videos",
+    "FANS"=>"Fans",
+    "NEWS"=>"News",
+    "SHOP"=>"Shop"]
+];
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+    <nav>
+        <div class="container">
+            <div class="row">
+                <!-- blue nav -->
+                <div class="col text-white ">
+                    <ul>
+                        <li class="nav-item py-1">
+                            <a class="nav-link" aria-current="page" href="#">DC PoweroTM VISA®</a>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <button class="btn  dropdown-toggle py-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    ADDITIONAL DC SITE
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">ADDITIONAL DC SITE 1</a></li>
+                                    <li><a class="dropdown-item" href="#">ADDITIONAL DC SITE 2</a></li>
+                                    <li><a class="dropdown-item" href="#">ADDITIONAL DC SITE 3</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <div class="navbar-brand">
+                        <a href="/">
+                            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                        </a>
+                    </div>
+                    <ul>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
